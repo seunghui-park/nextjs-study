959b52ac8de13a3030a5e27657588a77fc96ac7f
seunghui-park <ridia1233@naver.com>
2025-01-26 06:55:45 +0900

    5.쿼리 스트링 값 가져오기

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,6 +2,9 @@ import { useRouter } from "next/router"
 
 export default function Page(){
     const router = useRouter(); //router 변수에 라우터 객체 저장
-    console.log(router)
+    console.log(router);
+
+    //쿼리 스트링 값 불러오기
+    const {q} = router.query; // const q =router.query.q;랑 같음.
     return <h1>Search</h1>
 }
